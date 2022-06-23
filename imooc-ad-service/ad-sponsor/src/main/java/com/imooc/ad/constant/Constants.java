3b0342a915f1089f00d69b9b0c225bac5c5a80a0
hwenjun <66753253+hhhwenjun@users.noreply.github.com>
2022-06-22 22:26:27 -0400

    plan api and service impl commit

--- imooc-ad-service/ad-sponsor/src/main/java/com/imooc/ad/constant/Constants.java
+++ imooc-ad-service/ad-sponsor/src/main/java/com/imooc/ad/constant/Constants.java
@@ -8,5 +8,9 @@ public class Constants {
     public static class ErrorMsg {
         public static final String REQUEST_PARAM_ERROR = "Request param error!";
         public static final String SAME_NAME_ERROR = "User with same name already exists";
+        public static final String CAN_NOT_FIND_RECORD =
+                "Can not find the record";
+        public static final String SAME_NAME_PLAN_ERROR =
+                "Plan with same name already exists";
     }
 }
--- imooc-ad-service/ad-sponsor/src/main/java/com/imooc/ad/service/IAdPlanService.java
+++ imooc-ad-service/ad-sponsor/src/main/java/com/imooc/ad/service/IAdPlanService.java
@@ -1,4 +1,21 @@
 package com.imooc.ad.service;
 
+import com.imooc.ad.entity.AdPlan;
+import com.imooc.ad.exception.AdException;
+import com.imooc.ad.vo.AdPlanGetRequest;
+import com.imooc.ad.vo.AdPlanRequest;
+import com.imooc.ad.vo.AdPlanResponse;
+
+import java.util.List;
+
 public interface IAdPlanService {
+
+    /** Create the ad plan */
+    AdPlanResponse createAdPlan(AdPlanRequest request) throws AdException;
+    /** Obtain the ad plan */
+    List<AdPlan> getAdPlanByIds(AdPlanGetRequest request) throws AdException;
+    /** Update the ad plan */
+    AdPlanResponse updateAdPlan(AdPlanRequest request) throws AdException;
+    /** Delete the ad plan */
+    void deleteAdPlan(AdPlanRequest request) throws AdException;
 }
